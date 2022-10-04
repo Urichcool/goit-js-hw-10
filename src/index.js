b4bd967ee47db4f32f6049c41effc5b62aa5df3e
Urichcool <104870932+Urichcool@users.noreply.github.com>
2022-10-04 16:57:55 +0200

    task in progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
 
+const debounce = require('lodash.debounce');
+const searchInputEl = document.querySelector('#search-box');
 const DEBOUNCE_DELAY = 300;
+
+searchInputEl.addEventListener('input', debounce(() => {
+  if (searchInputEl.value !== "") {
+   const trimedValue = searchInputEl.value.trim();
+    fetchCountries();
+  };
+
+}, DEBOUNCE_DELAY));
